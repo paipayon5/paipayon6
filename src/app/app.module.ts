08981223af06a77de0029d2769a6b1f4bae147ad
PEAM <paipayon5@hotmail.com>
2019-02-17 10:44:58 +0700

    error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+imports:[BrowserModule,FormsModule,ReactiveFormsModule];
 @NgModule({
   declarations: [
     AppComponent
